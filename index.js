e62b11a323fabeea13eee070d86213881a05b592
AgentDan <bion-1@mail.ru>
2025-02-28 20:18:05 +0100

    com

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require("cors")
 const PORT = process.env.PORT || 5000
 connectDB()
 
+//
 app.use(cors())
 app.use(express.json({extended: true}))
 app.use('/api/', require('./routes/main'))
